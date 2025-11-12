070cb40def257c6de6d9edf3f391f62c61eeebb1
Rohit Kumar <rohit@Rohits-MacBook-Pro.local>
2025-11-12 10:17:14 +0530

    feat: Add final portfolio website and CI/CD pipeline

--- azure/project-1-static-site/terraform/main.tf
+++ azure/project-1-static-site/terraform/main.tf
@@ -1,5 +1,7 @@
-// 1. Tell Terraform we are using Azure
+// --- Provider & State Config ---
+// This block tells Terraform we're using Azure and where to store its "memory".
 terraform {
+  # We need the 'azurerm' provider to talk to Azure
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -7,52 +9,63 @@ terraform {
     }
   }
 
-  // 2. Tell Terraform where to save its "memory" (state file)
-  // This uses the storage account we made in Part B
+  # This is the remote backend. It's where Terraform saves its 'state file'.
+  # I'm using the 'oriax-automation-rg' storage account we made earlier.
   backend "azurerm" {
     resource_group_name  = "oriax-automation-rg"
-    storage_account_name = "oriaxterraformstate" 
+    storage_account_name = "oriaxterraformstate" # My unique name for the state storage
     container_name       = "tfstate"
-    key                  = "azure-p1-static-site.terraform.tfstate" 
+    key                  = "oriax-portfolio.terraform.tfstate" # State file for *this* project
   }
 }
 
-// 3. Configure the Azure Provider
+// Configure the Azure provider itself.
 provider "azurerm" {
   features {}
 }
 
-// 4. Create a Resource Group for this website
-resource "azurerm_resource_group" "site_rg" {
-  name     = "oriax-prod-site-rg"
-  location = "Central India" // <-- Set to your region
+
+// --- Website Infrastructure ---
+// This is the "blueprint" for the actual website resources.
+
+# 1. The main resource group for the website
+resource "azurerm_resource_group" "oriax_portfolio_rg" {
+  name     = "oriax-portfolio-website-rg"
+  location = "Central India"
 }
 
-// 5. Create the Storage Account to host the files
-resource "azurerm_storage_account" "site_storage" {
-  name                     = "oriaxprodportfoliosite" 
-  resource_group_name      = azurerm_resource_group.site_rg.name
-  location                 = azurerm_resource_group.site_rg.location
+# 2. The storage account that will host the HTML/CSS files
+resource "azurerm_storage_account" "oriax_portfolio_storage" {
+  # This name has to be globally unique
+  name                     = "oriaxprodportfoliosite" # My unique name
+  resource_group_name      = azurerm_resource_group.oriax_portfolio_rg.name
+  location                 = azurerm_resource_group.oriax_portfolio_rg.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "LRS" # LRS is cheapest and fine for a portfolio
 
-  // 6. This is the magic! This enables the "Static website" feature
+  # This block turns on the static website feature
   static_website {
     index_document     = "index.html"
     error_404_document = "error.html"
   }
 }
 
-// 7. Output the final website URL
-output "website_endpoint" {
-  value = azurerm_storage_account.site_storage.primary_web_endpoint
+
+// --- Outputs ---
+// These are values we want Terraform to print out after it's done.
+
+# The main URL for the live website
+output "website_url" {
+  value = azurerm_storage_account.oriax_portfolio_storage.primary_web_endpoint
 }
 
-// 8. Output the storage account name for the upload script
+# The name of the storage account, so our pipeline can upload files to it
 output "storage_account_name" {
-  value = azurerm_storage_account.site_storage.name
+  value = azurerm_storage_account.oriax_portfolio_storage.name
 }
+
+# The master key for the storage account, needed for the upload
 output "storage_account_primary_key" {
-  value     = azurerm_storage_account.site_storage.primary_access_key
-  sensitive = true
+  value     = azurerm_storage_account.oriax_portfolio_storage.primary_access_key
+  sensitive = true # Mark this as sensitive so it doesn't show in logs
 }
